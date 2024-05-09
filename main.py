2c3e43a386a4f114095a68c24777e91cf30c0979
Microglian <dominicfrench@gmail.com>
2024-05-09 09:14:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import data_access as data
+import processing as proc
+import user_interface as ui
+
+
+
+
+ui.root.mainloop()
---
+++ user_interface.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Honkai Star Tracks")
+root.geometry('1080x720+100+100')
+root.resizable(False, False)
+
